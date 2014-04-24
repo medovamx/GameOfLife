80d94dfc52158b89f030c51bfe5170ce2944626a
Mariya Medovar <medovamx@dukes.jmu.edu>
2014-04-24 17:46:35 -0400

    All neighbors are correct, core functionality complete

--- guiApplet/GameOfLifeGUI/src/Grid.java
+++ guiApplet/GameOfLifeGUI/src/Grid.java
@@ -20,20 +20,20 @@ public class Grid {
 
     //create new grid based on the old grid's cells
     public Grid(Grid g) {
+        byte aliveCount = 0;
         
         maxY = g.getHeight();
         maxX = g.getWidth();
         grid = new Grid(maxX, maxY).getGrid();
-        
-        System.out.println("CREATING GRID");
-        byte aliveCount = 0;
+       
         for (byte y = 0; y < maxY; y++) {
             for (byte x = 0; x < maxX; x++) {
                 aliveCount = g.getCell(x, y).getAliveNeighbors();
-                System.out.print(aliveCount + " ");               
+                
                 if (!g.getCell(x, y).checkLife() && aliveCount == 3) {
                     grid[y][x].setLife(true);
-                } else if (g.getCell(x, y).checkLife()) {
+                } 
+                else if (g.getCell(x, y).checkLife()) {
                     if (aliveCount < 2 || aliveCount > 3) {
                         grid[y][x].setLife(false);
                     }
@@ -42,97 +42,97 @@ public class Grid {
                     }
                 }               
             }
-            System.out.println(" ");
         }
     }
 
     //this function creates the corner cells' neighbors
     private void setCornerNeighbors() {
-        //bottom left-most corner
-        grid[0][0].addNeighbor(grid[maxY - 1][0]); //bottom neighbor
-        grid[0][0].addNeighbor(grid[0][maxX - 1]); //left neighbor
-        grid[0][0].addNeighbor(grid[0][1]); //right neighbor
-        grid[0][0].addNeighbor(grid[1][0]); //top neighbor
-        grid[0][0].addNeighbor(grid[1][1]); //top right diagonal
-        grid[0][0].addNeighbor(grid[1][maxX - 1]); //top left diagnonal
-        grid[0][0].addNeighbor(grid[maxY - 1][maxX - 1]); //bottom left diagonal
-        grid[0][0].addNeighbor(grid[maxY - 1][1]); //bottom right diagonal
-
-        //bottom right-most corner
-        grid[0][maxX - 1].addNeighbor(grid[maxY - 1][maxX - 1]); //bottom neighbor
-        grid[0][maxX - 1].addNeighbor(grid[0][maxX - 2]); //left neighbor
-        grid[0][maxX - 1].addNeighbor(grid[0][0]); //right neighbor
-        grid[0][maxX - 1].addNeighbor(grid[1][maxX - 1]); //top neighbor
-        grid[0][maxX - 1].addNeighbor(grid[0][1]); //top right diagonal
-        grid[0][maxX - 1].addNeighbor(grid[1][0]); //top left diagnonal
-        grid[0][maxX - 1].addNeighbor(grid[maxY - 1][maxX - 2]); //bottom left diagonal
-        grid[0][maxX - 1].addNeighbor(grid[0][maxX - 1]); //bottom right diagonal
-
         //top left-most corner
-        grid[maxY - 1][0].addNeighbor(grid[maxY - 2][0]); //bottom neighbor
-        grid[maxY - 1][0].addNeighbor(grid[maxY - 1][maxX - 1]); //left neighbor
-        grid[maxY - 1][0].addNeighbor(grid[maxY - 1][1]); //right neighbor
-        grid[maxY - 1][0].addNeighbor(grid[0][0]); //top neighbor
-        grid[maxY - 1][0].addNeighbor(grid[0][1]); //top right diagonal
-        grid[maxY - 1][0].addNeighbor(grid[0][maxX - 1]); //top left diagnonal
-        grid[maxY - 1][0].addNeighbor(grid[maxY - 2][maxX - 1]); //bottom left diagonal
-        grid[maxY - 1][0].addNeighbor(grid[maxY - 2][1]); //bottom right diagonal
+        grid[0][0].addNeighbor(grid[maxY - 1][0]); //top
+        grid[0][0].addNeighbor(grid[0][maxX - 1]); //left
+        grid[0][0].addNeighbor(grid[0][1]); //right
+        grid[0][0].addNeighbor(grid[1][0]); //bottom
+        grid[0][0].addNeighbor(grid[1][1]); //bottom right
+        grid[0][0].addNeighbor(grid[1][maxX - 1]); //bottom left
+        grid[0][0].addNeighbor(grid[maxY - 1][maxX - 1]); //top left
+        grid[0][0].addNeighbor(grid[maxY - 1][1]); //top right
 
         //top right-most corner
-        grid[maxY - 1][maxX - 1].addNeighbor(grid[maxY - 2][maxX - 1]); //bottom neighbor
-        grid[maxY - 1][maxX - 1].addNeighbor(grid[maxY - 1][maxX - 2]); //left neighbor
-        grid[maxY - 1][maxX - 1].addNeighbor(grid[maxY - 1][0]); //right neighbor
-        grid[maxY - 1][maxX - 1].addNeighbor(grid[0][maxX - 1]); //top neighbor
-        grid[maxY - 1][maxX - 1].addNeighbor(grid[0][0]); //top right diagonal
-        grid[maxY - 1][maxX - 1].addNeighbor(grid[0][maxX - 2]); //top left diagnonal
-        grid[maxY - 1][maxX - 1].addNeighbor(grid[maxY - 2][maxX - 2]); //bottom left diagonal
-        grid[maxY - 1][maxX - 1].addNeighbor(grid[maxY - 2][0]); //bottom right diagonal
+        grid[0][maxX - 1].addNeighbor(grid[maxY - 1][maxX - 1]); //top
+        grid[0][maxX - 1].addNeighbor(grid[0][maxX - 2]); //left
+        grid[0][maxX - 1].addNeighbor(grid[0][0]); //right
+        grid[0][maxX - 1].addNeighbor(grid[1][maxX - 1]); //bottom
+        grid[0][maxX - 1].addNeighbor(grid[1][0]); //bottom right
+        grid[0][maxX - 1].addNeighbor(grid[maxY - 1][maxX - 2]); //top left
+        grid[0][maxX - 1].addNeighbor(grid[1][maxX - 2]); //bottom left
+        grid[0][maxX - 1].addNeighbor(grid[maxY - 1][0]); //top right
+
+        //bottom left-most corner
+        grid[maxY - 1][0].addNeighbor(grid[maxY - 2][0]); //top
+        grid[maxY - 1][0].addNeighbor(grid[maxY - 1][maxX - 1]); //left
+        grid[maxY - 1][0].addNeighbor(grid[maxY - 1][1]); //right
+        grid[maxY - 1][0].addNeighbor(grid[0][0]); //bottom
+        grid[maxY - 1][0].addNeighbor(grid[0][1]); //bottom right
+        grid[maxY - 1][0].addNeighbor(grid[0][maxX - 1]); //bottom left
+        grid[maxY - 1][0].addNeighbor(grid[maxY - 2][maxX - 1]); //top left
+        grid[maxY - 1][0].addNeighbor(grid[maxY - 2][1]); //top right
+
+        //bottom right-most corner
+        grid[maxY - 1][maxX - 1].addNeighbor(grid[maxY - 2][maxX - 1]); //top
+        grid[maxY - 1][maxX - 1].addNeighbor(grid[maxY - 1][maxX - 2]); //left
+        grid[maxY - 1][maxX - 1].addNeighbor(grid[maxY - 1][0]); //right
+        grid[maxY - 1][maxX - 1].addNeighbor(grid[0][maxX - 1]); //bottom
+        grid[maxY - 1][maxX - 1].addNeighbor(grid[0][0]); //bottom right
+        grid[maxY - 1][maxX - 1].addNeighbor(grid[0][maxX - 2]); //bottom left
+        grid[maxY - 1][maxX - 1].addNeighbor(grid[maxY - 2][maxX - 2]); //top left
+        grid[maxY - 1][maxX - 1].addNeighbor(grid[maxY - 2][0]); //top right
     }
 
     //This function does not include corners
     private void setEdgeNeighbors() {
         for (byte y = 1; y < maxY - 1; y++) {
             //left edge
-            grid[y][0].addNeighbor(grid[y - 1][0]); //bottom neighbor
-            grid[y][0].addNeighbor(grid[y][maxX - 1]); //left neighbor
-            grid[y][0].addNeighbor(grid[y][1]); //right neighbor
-            grid[y][0].addNeighbor(grid[y + 1][0]); //top neighbor
-            grid[y][0].addNeighbor(grid[y + 1][1]); //top right diagonal
-            grid[y][0].addNeighbor(grid[y + 1][maxX - 1]); //top left diagnonal
-            grid[y][0].addNeighbor(grid[y - 1][maxX - 1]); //bottom left diagonal
-            grid[y][0].addNeighbor(grid[y - 1][1]); //bottom right diagonal
+            grid[y][0].addNeighbor(grid[y - 1][0]); //top
+            grid[y][0].addNeighbor(grid[y][maxX - 1]); //left
+            grid[y][0].addNeighbor(grid[y][1]); //right
+            grid[y][0].addNeighbor(grid[y + 1][0]); //bottom
+            grid[y][0].addNeighbor(grid[y + 1][1]); //bottom right
+            grid[y][0].addNeighbor(grid[y + 1][maxX - 1]); //bottom left
+            grid[y][0].addNeighbor(grid[y - 1][maxX - 1]); //top left
+            grid[y][0].addNeighbor(grid[y - 1][1]); //top right
 
             //right edge
-            grid[y][maxX - 1].addNeighbor(grid[y - 1][maxX - 1]); //bottom neighbor
-            grid[y][maxX - 1].addNeighbor(grid[y][maxX - 2]); //left neighbor
-            grid[y][maxX - 1].addNeighbor(grid[y][0]); //right neighbor
-            grid[y][maxX - 1].addNeighbor(grid[y + 1][maxX - 1]); //top neighbor
-            grid[y][maxX - 1].addNeighbor(grid[y + 1][1]); //top right diagonal
-            grid[y][maxX - 1].addNeighbor(grid[y + 1][0]); //top left diagnonal
-            grid[y][maxX - 1].addNeighbor(grid[y - 1][maxX - 2]); //bottom left diagonal
-            grid[y][maxX - 1].addNeighbor(grid[y - 1][maxX - 1]); //bottom right diagonal
+            grid[y][maxX - 1].addNeighbor(grid[y - 1][maxX - 1]); //top
+            grid[y][maxX - 1].addNeighbor(grid[y][maxX - 2]); //left
+            grid[y][maxX - 1].addNeighbor(grid[y][0]); //right
+            grid[y][maxX - 1].addNeighbor(grid[y + 1][maxX - 1]); //bottom
+            grid[y][maxX - 1].addNeighbor(grid[y + 1][0]); //bottom right
+            grid[y][maxX - 1].addNeighbor(grid[y - 1][maxX - 2]); //top left
+            grid[y][maxX - 1].addNeighbor(grid[y + 1][maxX - 2]); //bottom left
+            grid[y][maxX - 1].addNeighbor(grid[y - 1][0]); //top right     
         }
 
         for (byte x = 1; x < maxX - 1; x++) {
-            //bottom edge
-            grid[0][x].addNeighbor(grid[maxY - 1][x]); //bottom neighbor
-            grid[0][x].addNeighbor(grid[0][x - 1]); //left neighbor
-            grid[0][x].addNeighbor(grid[0][x + 1]); //right neighbor
-            grid[0][x].addNeighbor(grid[1][x]); //top neighbor
-            grid[0][x].addNeighbor(grid[1][x + 1]); //top right diagonal
-            grid[0][x].addNeighbor(grid[1][x - 1]); //top left diagnonal
-            grid[0][x].addNeighbor(grid[maxY - 1][x - 1]); //bottom left diagonal
-            grid[0][x].addNeighbor(grid[maxY - 1][x + 1]); //bottom right diagonal
-
             //top edge
-            grid[maxY - 1][x].addNeighbor(grid[maxY - 2][x]); //bottom neighbor
-            grid[maxY - 1][x].addNeighbor(grid[maxY - 1][x - 1]); //left neighbor
-            grid[maxY - 1][x].addNeighbor(grid[maxY - 1][x + 1]); //right neighbor
-            grid[maxY - 1][x].addNeighbor(grid[0][x]); //top neighbor
-            grid[maxY - 1][x].addNeighbor(grid[0][x + 1]); //top right diagonal
-            grid[maxY - 1][x].addNeighbor(grid[0][x - 1]); //top left diagnonal
-            grid[maxY - 1][x].addNeighbor(grid[maxY - 2][x - 1]); //bottom left diagonal
-            grid[maxY - 1][x].addNeighbor(grid[maxY - 2][x + 1]); //bottom right diagonal
+            grid[0][x].addNeighbor(grid[maxY - 1][x]); //top
+            grid[0][x].addNeighbor(grid[0][x - 1]); //left
+            grid[0][x].addNeighbor(grid[0][x + 1]); //right
+            grid[0][x].addNeighbor(grid[maxY - 1][x - 1]); //top left
+            grid[0][x].addNeighbor(grid[maxY - 1][x + 1]); //top right
+            grid[0][x].addNeighbor(grid[1][x - 1]); //bottom left
+            grid[0][x].addNeighbor(grid[1][x]); //bottom
+            grid[0][x].addNeighbor(grid[1][x + 1]); //bottom right
+            
+
+            //bottom edge
+            grid[maxY - 1][x].addNeighbor(grid[maxY - 2][x]); //top
+            grid[maxY - 1][x].addNeighbor(grid[maxY - 1][x - 1]); //left
+            grid[maxY - 1][x].addNeighbor(grid[maxY - 1][x + 1]); //right
+            grid[maxY - 1][x].addNeighbor(grid[0][x]); //bottom
+            grid[maxY - 1][x].addNeighbor(grid[0][x + 1]); //bottom right
+            grid[maxY - 1][x].addNeighbor(grid[0][x - 1]); //bottom left
+            grid[maxY - 1][x].addNeighbor(grid[maxY - 2][x - 1]); //top left
+            grid[maxY - 1][x].addNeighbor(grid[maxY - 2][x + 1]); //top right
         }
 
     }
